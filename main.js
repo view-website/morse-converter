98337186a3179036106612f31ee7fd721f0da07b
view-website <91933827+view-website@users.noreply.github.com>
2024-06-01 15:42:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,119 @@
+var input;
+var input_text;
+function generate(){
+    var input_text = document.getElementById("input-txt").value;
+    document.getElementById("out-txt").innerHTML = input_text;
+    //if (document.getElementById("out-txt").value.includes("a") === "true"){
+    //if (word.includes("a")){
+    for (let i = 0; i < 30; i++){
+    document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("a", "·- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("b", "-··· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("c", "-·-· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("d", "-·· ");
+    }
+    for (let i = 0; i < 30; i++){
+            document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("e", "· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("f", "··-· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("g", "--· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("h", "···· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("i", "·· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("j", "·--- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("k", "-·- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("l", "·-·· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("m", "-- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("n", "-· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("o", "--- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("p", "·--· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("q", "--·- ");
+    }
+    for (let i = 0; i < 30; i++){ 
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("r", "·-· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("s", "··· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("t", "- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("u", "··- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("v", "···- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("w", "·-- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("x", "-··- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("y", "-·-- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("z", "--·· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("  ", "/");
+    }
+       for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("1", "·---- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("2", "··--- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("3", "···-- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("4", "····- ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("5", "····· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("6", "-···· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("7", "--··· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("8", "---·· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("9", "----· ");
+    }
+    for (let i = 0; i < 30; i++){
+        document.getElementById("out-txt").innerHTML = document.getElementById("out-txt").innerHTML.replace("0", "----- ");
+    }
+}
